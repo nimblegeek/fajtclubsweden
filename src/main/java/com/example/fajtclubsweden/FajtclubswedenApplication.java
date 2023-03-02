3d28b27068a5f6ba6db7240fdaef8f50ee431b71
Jonas A Sihlén <jonasasihlen@gmail.com>
2023-03-02 10:15:33 +0100

    Project structure

---
+++ src/main/java/com/example/fajtclubsweden/FajtclubswedenApplication.java
@@ -0,0 +1,13 @@
+package com.example.fajtclubsweden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FajtclubswedenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FajtclubswedenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fajtclubsweden/FajtclubswedenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fajtclubsweden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FajtclubswedenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
